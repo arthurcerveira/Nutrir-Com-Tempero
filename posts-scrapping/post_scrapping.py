aba28cc58fd397396054197ab65cf3c60700a327
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-07-03 20:44:17 -0300

    Remove services section and add python script to scrap posts

---
+++ posts-scrapping/post_scrapping.py
@@ -0,0 +1,34 @@
+import unicodedata
+
+from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+URL = 'https://nutrircomtempero.wordpress.com/'
+
+
+@app.route('/scrap_posts', methods=['GET'])
+def scrap_posts():
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    posts = soup.find_all('article', class_='post')
+
+    response = dict()
+
+    # Get the first three posts
+    for post_index in range(3):
+        post = posts[post_index]
+
+        title_string = post.find('h2', class_='entry-title').text
+
+        title = unicodedata.normalize('NFKC', title_string)
+        img = post.find('img')['src']
+        link = post.find('a')['href']
+
+        response[f'post{post_index}'] = {'title': title,
+                                         'img': img,
+                                         'link': link}
+
+    return response
